75f70d5317a1125f4e5d0d35e40e1c9d2dcdac24
David-Benitez <79118508+David-Benitez@users.noreply.github.com>
2024-04-07 18:44:42 -0300

    Se agregan componentes de prueba

---
+++ components/Buttons2.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Button} from "@nextui-org/react";
+
+const Buttons2 = () => {
+  return (
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="default">
+        Default
+      </Button>
+      <Button color="primary">
+        Primary
+      </Button>
+      <Button color="secondary">
+        Secondary
+      </Button>
+      <Button color="success">
+        Success
+      </Button>
+      <Button color="warning">
+        Warning
+      </Button>
+      <Button color="danger">
+        Danger
+      </Button>
+    </div>
+  );
+}
+
+export default Buttons2;
